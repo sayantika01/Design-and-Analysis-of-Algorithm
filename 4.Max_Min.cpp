dd6218f9a3f8510914800026a3399f73d2a5077f
Sayantika Kar <sayantikakar608@gmail.com>
2024-05-22 09:25:21 +0530

    day2 commit2

--- 4.Max_Min.cpp
+++ 4.Max_Min.cpp
@@ -1,10 +1,10 @@
 #include<bits/stdc++.h>
 using namespace std;
-void max_min(int a[],int i,int j,int maxi,int mini){
+void max_min(int a[],int i,int j,int &maxi,int &mini){
     if(i==j){
         maxi=mini=a[i];
     }
-    else if(i=j-1){
+    else if(i==j-1){
         if(a[i]>a[j]){
             maxi=a[i];
             mini=a[j];
@@ -16,25 +16,23 @@ void max_min(int a[],int i,int j,int maxi,int mini){
     }
     else{
         int mid=(i+j)/2;
-        int maxi2,mini2;
-        max_min(a,i,mid,maxi,mini);
+        int maxi2,mini2,maxi1,mini1;
+        max_min(a,i,mid,maxi1,mini1);
         max_min(a,mid+1,j,maxi2,mini2);
-        if(maxi2>maxi)
-            maxi=maxi2;
-        if(mini2<mini)
-            mini=mini2;
+        maxi=max(maxi1,maxi2);
+        mini=min(mini1,mini2);
     }
 }
 int main(){
-    int n,i,maxi,mini;
+    int n;
     cout<<"Enter the numner of elements in the array: "<<endl;
     cin>>n;
     int arr[n];
     cout<<"Enter the elements in the array: "<<endl;
-    for(i=0;i<n;i++){
+    for(int i=0;i<n;i++){
         cin>>arr[i];
     }
-    cout<<"The maximum value and the minimum value in the array are: ";
-    max_min(arr,i,n-1,maxi,mini);
-    cout<<"Respectively"<<endl;
+    int maxi,mini;
+    max_min(arr,0,n-1,maxi,mini);
+    cout<<"The maximum value and the minimum value in the array are: "<<maxi<<" and "<<mini<<"Respectively"<<endl;
 }
